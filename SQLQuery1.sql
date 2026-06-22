e2b20276f54f4dddaa4c1f3ef5015cd0ef80c091
Solomon Tsega <tsegasolomon538@gmail.com>
2026-06-22 20:13:59 +0300

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,9 +0,0 @@
---CREATE DATABASE PracticeDB;
---CREATE TABLE Persons ( 
---PersonID int PRIMARY KEY, 
---LastName varchar(255) NOT NULL, 
---FirstName varchar(255), 
---Address varchar(255), 
---City varchar(255) 
---); 
-
